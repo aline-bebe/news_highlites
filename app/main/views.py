405652c0acee41ddca31a9b0fda194070cf70694
aline-bebe <ingabirealine40@gmail.com>
2019-09-13 17:33:44 +0200

    creating app instance and installing flask script

---
+++ app/main/views.py
@@ -0,0 +1,24 @@
+from flask import render_template, request, redirect, url_for
+from . import main
+from ..request import get_source, get_articles
+from ..models import Source, Article
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    news_source = get_source()
+    
+    title = "Welcome to News Highlight"
+    return render_template('index.html', title = title, sources = news_source)
+
+@main.route('/articles/<articles_id>')
+def articles(articles_id):
+    '''
+    View articles page function that returns artcicles details and its data
+    '''
+    articles = get_articles()
+    return render_template('articles.html',  articles = articles)
